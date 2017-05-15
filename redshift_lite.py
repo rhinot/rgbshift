da789a2b594c9b9cc8aebe76467508c876c03cf6
Ryan T <ryant.code@gmail.com>
2017-05-14 21:19:51 -0400

    Initial redshift_lite
    Initial checkin of my version of redshift, which just provides current temperature and brightness, based on elevation of the sun.

---
+++ redshift_lite.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+
+"""redshift_lite.py: A light version of redshift, written in Python"""
+# For more information on redshift, see https://github.com/jonls/redshift/
+
+__author__      = "Ryan Tabone"
+__copyright__   = "Copyright MMXVII, Ryan Tabone"
+
+##### IMPORTS #####
+# Import 1st party
+import json
+import math
+from datetime import datetime
+
+# Import 3rd party
+import requests
+from astral import Astral
+#import params
+
+
+
+
+
+##### DEFAULT VARIABLES #####
+# Default values for temperature & brightness.
+DEFAULT_DAY_TEMP =  5500
+DEFAULT_NIGHT_TEMP = 3500
+DEFAULT_BRIGHTNESS = 1.0
+DEFAULT_DAY_BRIGHTNESS = DEFAULT_BRIGHTNESS
+DEFAULT_NIGHT_BRIGHTNESS = 0.5
+
+# The color temperature when no adjustment is applied.
+NEUTRAL_TEMP = 6500
+
+# Solar variables
+# Model of atmospheric refraction near horizon (in degrees).
+SOLAR_ATM_REFRAC = 0.833
+
+SOLAR_ASTRO_TWILIGHT_ELEV = -18.0
+SOLAR_NAUT_TWILIGHT_ELEV  = -12.0
+SOLAR_CIVIL_TWILIGHT_ELEV = -6.0
+SOLAR_DAYTIME_ELEV = (0.0 - SOLAR_ATM_REFRAC)
+
+
+# Angular elevation of the sun at which the color temperature
+# transition period starts and ends (in degress).
+# Transition during twilight, and while the sun is lower than
+# 3.0 degrees above the horizon.
+
+TRANSITION_LOW = SOLAR_CIVIL_TWILIGHT_ELEV
+TRANSITION_HIGH =  3.0
+
+# Duration of sleep between screen updates (milliseconds).
+SLEEP_DURATION = 5000
+SLEEP_DURATION_SHORT = 100
+
+
+
+
+
+##### CLASS DEFINITIONS #####
+# Class for storing location in latitude and longitude
+class location:
+	def __init__(self):
+		self.lat = None
+		self.lon = None
+
+# Class for storing temperature, gamma & brightness at time t
+class color_setting_t:
+	def __init__(self):
+		self.temperature = None
+		# self.gamma = [DEFAULT_GAMMA] * 3
+		self.brightness = DEFAULT_BRIGHTNESS
+
+class transition_scheme_t:
+	def __init__(self):
+		self.high = TRANSITION_HIGH
+		self.low = TRANSITION_LOW
+		self.day = color_setting_t()
+		self.day.temperature = DEFAULT_DAY_TEMP
+		self.night = color_setting_t()
+		self.night.temperature = DEFAULT_NIGHT_TEMP
+		self.night.brightness = DEFAULT_NIGHT_BRIGHTNESS
+
+
+
+
+
+##### FUNCTIONS #####
+# Conversion to radians
+def RAD (num) :
+	return (num*(math.pi/180))
+
+def CLAMP (n,low,high) :
+	return max(low, min(n,high))
+
+###################
+# Get current latitude and longitude, returned in tuple - [0] = lat; [1] = lon
+def getlatlon (location) :
+	send_url = 'http://freegeoip.net/json'
+	r = requests.get(send_url)
+	j = json.loads(r.text)
+
+	location.lat = j['latitude']
+	print location.lat
+	location.lon = j['longitude']
+	print location.lon
+
+	return (location)
+
+
+
+###################
+# Determine what the color settings should be during transition
+def interpolate_color_settings (transition, elevation, result) :
+	day = transition.day
+	night = transition.night
+
+	alpha = (transition.low - elevation) / (transition.low - transition.high)
+	alpha = CLAMP(alpha,0.0,1.0)
+
+	result.temperature = (1.0-alpha) * night.temperature + alpha * day.temperature
+	result.brightness = (1.0-alpha) * night.brightness + alpha * day.brightness
+	# for x in range(3) :
+	#	result.gamma[x] = (1.0-alpha) * night.gamma[x] + alpha * day.gamma[x]
+
+
+
+
+##### MAIN #####
+def get_current_color() :
+	# Get current angular elevation of the sun
+	loc = getlatlon (location())
+	sol_elev = Astral.solar_elevation(Astral(),datetime.utcnow(),loc.lat,loc.lon)
+
+	print sol_elev
+
+	# Use elevation of sun to set color temperature
+	scheme = transition_scheme_t()
+	interp = color_setting_t()
+	print interp.temperature
+	print interp.brightness
+	interpolate_color_settings (scheme,sol_elev,interp)
+
+	
+
+	return (interp.temperature, interp.brightness)
+
+test_vars = get_current_color()
+
+print test_vars[0]
+print test_vars[1]
+
+
